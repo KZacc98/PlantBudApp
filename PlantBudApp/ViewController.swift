17d622ad5e1da1fc6cb25554ed1006d44938ac10
Kamil Zachara <kamil.zachara98@gmail.com>
2022-11-19 23:12:42 +0100

    init, no storyboards

--- PlantBudApp/ViewController.swift
+++ PlantBudApp/ViewController.swift
@@ -8,10 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    lazy var test: UILabel = {
+        let test = UILabel()
+        test.text = "klasjdhlkjfsda"
+        test.translatesAutoresizingMaskIntoConstraints = false
+        test.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        test.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        test.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        test.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        
+        
+        view.addSubview(test)
+        
+        return test
+    }()
 
     override func viewDidLoad() {
+        
+        let test = UILabel()
+        test.text = "klasjdhlkjfsda"
+        view.addSubview(test)
+        test.translatesAutoresizingMaskIntoConstraints = false
+        test.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        test.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        
+        
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
 
 
